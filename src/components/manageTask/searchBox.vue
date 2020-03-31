9e96f4a2c33977fd627d28c7c9d558178d69adec
Seway <47681078+Seway@users.noreply.github.com>
2020-03-31 11:27:04 +0800

    第一次提交
    使用vue-cli脚手架创建项目，新增搜索栏组件

---
+++ src/components/manageTask/searchBox.vue
@@ -0,0 +1,79 @@
+<template>
+  <form class="searchBoxForm" role="form">
+    <div class="row">
+      <div class="col-xs-2">
+        <div class="input-group">
+          <input type="text" class="form-control" />
+          <div class="input-group-btn">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+              下拉菜单
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu pull-right">
+              <li>
+                <a href="#">功能</a>
+              </li>
+              <li>
+                <a href="#">另一个功能</a>
+              </li>
+              <li>
+                <a href="#">其他</a>
+              </li>
+            </ul>
+          </div>
+          <!-- /btn-group -->
+        </div>
+        <!-- /input-group -->
+      </div>
+      <!-- /.col-lg-6 -->
+      <div class="diver"></div>
+      <div class="col-xs-2">
+        <div class="input-group">
+          <input type="text" class="form-control" />
+          <div class="input-group-btn">
+            <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+              下拉菜单
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu pull-right">
+              <li>
+                <a href="#">功能</a>
+              </li>
+              <li>
+                <a href="#">另一个功能</a>
+              </li>
+              <li>
+                <a href="#">其他</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+<div class="diver"></div>
+      <div class="col-xs-2">
+        <div class="input-group">
+          <input type="text" class="form-control" />
+        </div>
+      </div>
+<div class="diver"></div>
+      <div class="col-xs-2">
+        <div class="input-group">
+          <button type="button" class="btn btn-default">搜索</button>
+        </div>
+      </div>
+    </div>
+    <!-- /.row -->
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'serachBox'
+}
+</script>
+
+<style>
+.searchBoxForm{
+  border: 1px solid black;
+}
+</style>
---
+++ src/views/manageTask.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="manageTask">
+    <h1>任务管理页面</h1>
+    <searchBox></searchBox>
+  </div>
+</template>
+
+<script>
+import searchBox from '@/components/manageTask/searchBox.vue'
+
+export default {
+  name: 'pageTask',
+  components: {
+    searchBox
+  }
+}
+</script>
+
+<style>
+.manageTask{
+  background: white;
+  border: 1px solid black;
+}
+</style>
